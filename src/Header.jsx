3b23c1c8a6b30e1f15894f13997f6305c027f3dc
hamza-140 <sahamzashah19@gmail.com>
2024-05-07 18:36:21 +0500

    update

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,23 +1,24 @@
-import React from 'react';
 import profilePic from '../src/assets/me.jpg';
 const Header = () => {
   return (
-    <header className="flex flex-col items-center justify-center sm:flex-row sm:justify-between p-4">
-      <div className="text-center sm:text-left sm:w-1/2">
-        <h1 className="text-2xl font-bold mb-2">Hamza Syed</h1>
-        <p className="text-sm text-gray-600 max-w-md">
-          Technophile learning the ways to come out on top 🚀.
-        </p>
-      </div>
+    <header className='flex'>
+  <div className="flex ml-auto mr-auto flex-col items-center justify-center p-4 text-center">
+    <h1 className="text-2xl font-bold mb-2 text-center">Hamza Syed</h1>
+    <p className="text-sm text-gray-600 text-center">
+      Technophile learning the ways to come out on top 🚀.
+    </p>
+  </div>
 
-      <div className="sm:ml-auto">
-        <img 
-          src={profilePic} 
-          alt="Profile" 
-          className="rounded-full w-24 h-24 object-cover"
-        />
-      </div>
-    </header>
+  <div className="flex justify-end">
+    <img 
+      src={profilePic} 
+      alt="Profile" 
+      style={{height:'150px',width:'150px',marginRight:'30px', marginTop:'5px',marginBottom:'5px'}}
+      className="rounded-full w-24 h-24 object-cover"
+    />
+  </div>
+</header>
+
   );
 }
 
